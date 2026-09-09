bab3345eb98983973911ec295cdaadb81ea1de51
Ranbot1 <2574210390@qq.com>
2026-09-09 12:07:59 +0800

    repro: export dual-impulse kinematic helpers

--- reproductions/2025_RIE_PhysicsTeacher/src/fmdtl/sim/__init__.py
+++ reproductions/2025_RIE_PhysicsTeacher/src/fmdtl/sim/__init__.py
@@ -4,6 +4,10 @@ from .bearing_4dof import (
     FaultParameters,
     SimulationParameters,
     characteristic_frequencies,
+    cage_speed_rad_s,
+    defect_passage_events,
+    dual_impulse_spacing_s,
+    fault_relative_speed_rad_s,
     simulate_bearing,
 )
 
@@ -13,5 +17,9 @@ __all__ = [
     "FaultParameters",
     "SimulationParameters",
     "characteristic_frequencies",
+    "cage_speed_rad_s",
+    "defect_passage_events",
+    "dual_impulse_spacing_s",
+    "fault_relative_speed_rad_s",
     "simulate_bearing",
 ]
