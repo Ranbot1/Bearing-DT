11dc24f986bca6eae90689b360d6d82f2d6e0b4e
Ranbot1 <2574210390@qq.com>
2026-09-09 10:41:17 +0800

    repro: add reproductions/2025_RIE_PhysicsTeacher/src/fmdtl/sim/__init__.py

---
+++ reproductions/2025_RIE_PhysicsTeacher/src/fmdtl/sim/__init__.py
@@ -0,0 +1,17 @@
+from .bearing_4dof import (
+    BearingGeometry,
+    DynamicParameters,
+    FaultParameters,
+    SimulationParameters,
+    characteristic_frequencies,
+    simulate_bearing,
+)
+
+__all__ = [
+    "BearingGeometry",
+    "DynamicParameters",
+    "FaultParameters",
+    "SimulationParameters",
+    "characteristic_frequencies",
+    "simulate_bearing",
+]
